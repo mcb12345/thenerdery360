6a7e5bb565a547934d5969f2116c542ab5a455d8
unknown <mbungert@mbungert-win7pr.nerdery.ad.bravo>
2014-10-27 14:09:52 -0500

    removed unused files.

--- src/main/webapp/resources/vote.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `vote` (
-  `vote_id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_id` int(11) NOT NULL,
-  `created` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`vote_id`),
-  KEY `id_idx` (`game_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
